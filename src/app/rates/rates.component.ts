2db105efc0a940923adf0dc0ff156d122bb36acd
Dylan Chung <dylan.chung1997@gmail.com>
2020-08-12 20:12:53 -0700

    Retrieved "rate", "name" [ ] and combined it.

--- src/app/rates/rates.component.ts
+++ src/app/rates/rates.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-rates',
@@ -7,9 +8,32 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class RatesComponent implements OnInit {
 
+  @Input() selectedGachapon: string; //Passed to us from gacha-UI 
+  rateNameList: string[];
+
   constructor() { }
 
   ngOnInit(): void {
+    axios.get("http://localhost:5000/gacha/" + this.selectedGachapon)
+      .then(res => {
+        console.log(res.data) //"rate" "name"
+        this.rateNameList = this.combineToProperFormat(res.data);
+      });
+  }
+
+  //Combine tha array from "rate","name" format to "rate name" format
+  combineToProperFormat(resdata){
+    let rateNameList = [];
+
+    for(let i = 0; i<= resdata.length-2 ; i+=2){
+      //Combine index two at a time
+      rateNameList[i] = resdata[i] + "\t" + resdata[i+1];
+    }
+
+    return rateNameList;
+
   }
 
 }
+
+
