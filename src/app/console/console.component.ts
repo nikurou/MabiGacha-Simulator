8083813764443d3ed336f9c4822c010fc71b38b4
Dylan Chung <dylan.chung1997@gmail.com>
2020-08-12 20:13:45 -0700

    Formatting and deleted unused methods

--- src/app/console/console.component.ts
+++ src/app/console/console.component.ts
@@ -8,18 +8,11 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ConsoleComponent implements OnInit {
 
   @Input() resultGach: string[]; //Passed to us from gacha-ui
-  public localResultGach: string[];
 
   constructor() {
   }
 
-  //Append localResultGach with the resultGach passed to us from server.
-  append(){
-   
-  }
-
   ngOnInit(): void {
- 
   }
 
 }
