bcfb3f746d8dd817aa0410baa053fe53464a7e81
Dylan Chung <dylan.chung1997@gmail.com>
2020-08-14 06:18:11 -0700

    Slowmode logic and autoscroll logic added

--- src/app/console/console.component.ts
+++ src/app/console/console.component.ts
@@ -11,18 +11,44 @@ export class ConsoleComponent implements OnInit {
   @ViewChildren('item') itemElements: QueryList<any>;
 
   private scrollContainer: any;
-  @Input() resultGach: []; //Passed to us from gacha-ui
+  private slowEnabled: boolean;
+  //@Input() resultGach: string[]; //Passed to us from gacha-ui [resultGach] = "resultGach"
+
+  resultGach: string[];
+  @Input() currResultGach: string[]; //Passed to us from gacha-ui represents latest gacha results.
+  timeInterval: number; //time between each interval update
 
   constructor() {
+    this.slowEnabled = false;
+    this.resultGach = [];
   }
 
   ngOnInit(): void {
-   
+    
   }
 
   ngAfterViewInit() {
     this.scrollContainer = this.scrollFrame.nativeElement;  
-    this.itemElements.changes.subscribe(_ => this.onItemElementsChanged());    
+    this.itemElements.changes.subscribe(_ => this.onItemElementsChanged());  
+    
+   
+  }
+
+  // Push the results to the array that is displayed on screen either slow or fast
+  // Depends on what user toggled.
+  public gachPushResults(){
+    let i = 0;
+    let stopIndex: number;
+    let interval = this.timeInterval
+    let thisUpdater = setInterval(() => {
+      stopIndex = this.currResultGach.length; 
+      this.resultGach.push(this.currResultGach[i]);
+      i = i+ 1;
+      
+      if(i == stopIndex){
+        clearInterval(thisUpdater); //Stops the continual updating on intervals when this is called
+      }
+    }, interval); //If slowmode slider is toggled, the time interval shortens
   }
 
   private onItemElementsChanged(): void {
@@ -35,6 +61,16 @@ export class ConsoleComponent implements OnInit {
       left: 0,
       behavior: 'smooth'
     });
+  }
+
+  //Slow down the rate at which items are shown
+  public toggleSlowMode(){
+    this.slowEnabled = !this.slowEnabled;
+    if(this.slowEnabled == true){
+      this.timeInterval = 500; //500ms
+    }else{
+      this.timeInterval = 1; //instant almost 
+    }
   }
 
   /*--AutoScroll from this tutorial: 
